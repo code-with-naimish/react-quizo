a61aa8489fb3577665045ecbba37b76c791deddd
code-with-naimish <naimish028@gmail.com>
2025-01-20 21:13:14 +0530

    progress

--- src/components/initial-screen.tsx
+++ src/components/initial-screen.tsx
@@ -14,20 +14,47 @@ const categories =
     { label: "General Knowledge", value: "general_knowledge" }
   ]
 
+const difficulty = [
+  { label: "Easy", value: "easy" },
+  { label: "Medium", value: "medium" },
+  { label: "Hard", value: "hard" },
+]
+
+const type = [
+  { label: "Text_choice", value: "text_choice" },
+  { label: "Image_choice", value: "image_choice" },
+]
 
 export default function InitialScreen() {
   return (
     <section className="card">
-      <h1>Welcome to Quizo!</h1>
-      <p>A fun and interactive quiz app designed to challenge your knowledge across various topics! Compete with friends, explore trivia, and track your progress as you learn something new every day.</p>
+      <h1 className="text-xl font-semibold">Welcome to Quizo!</h1>
+      <p className="my-1.5 text-sm">A fun and interactive quiz app designed to challenge your knowledge across various topics! Compete with friends, explore trivia, and track your progress as you learn something new every day.</p>
       <div>
-        <h2>Category</h2>
+        <h2 className="mb-2 font-medium text-base">Category:</h2>
         <ul className="flex gap-3 flex-wrap items-center ">
           {categories.map((category, i) => {
-            return <li className="bg-cyan-500/5 border border-cyan-300  font-medium  text-cyan-500 text-sm px-4 py-1 rounded-[99px]" key={i}>{category.label}
+            return <li className=" border border-stone-300  font-medium text-xs px-4 py-1 rounded-[99px]" key={i}>{category.label}
             </li>
           })}
         </ul>
+
+        <h2 className="my-1.5 font-medium">Difficulty:</h2>
+        <ul className="flex gap-3 flex-wrap items-center ">
+          {difficulty.map((difficulty, i) => {
+            return <li className="border border-stone-300  font-medium  text-xs px-4 py-1 rounded-[99px]" key={i}>{difficulty.label}
+            </li>
+          })}
+        </ul>
+
+        <h2 className="my-1.5 font-medium">Type:</h2>
+        <ul className="flex gap-3 flex-wrap items-center ">
+          {type.map((type, i) => {
+            return <li className="border border-stone-300  font-medium  text-xs px-4 py-1 rounded-[99px]" key={i}>{type.label}
+            </li>
+          })}
+        </ul>
+
       </div>
 
       <PrimaryBtn title="Let's go!" />
--- src/components/primary-btn.tsx
+++ src/components/primary-btn.tsx
@@ -3,7 +3,7 @@ export default function PrimaryBtn(props: {
   title: string
 }) {
   return (
-    <button className=" bg-gradient-to-r from-cyan-500 to-blue-500 hover:opacity-90 text-white px-6 py-2 rounded-[99px] transition-all ">{props.title}</button>
+    <button className=" bg-gradient-to-r from-cyan-500 to-blue-500 hover:opacity-90 text-white px-6 py-2 rounded-[99px] transition-all mt-4">{props.title}</button>
 
   )
 }
