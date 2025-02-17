0a1dc3eb8de34243650c794e230d105e4183aecc
code-with-naimish <naimish028@gmail.com>
2025-02-17 22:10:34 +0530

    progress

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { PiLightbulbFilament } from "react-icons/pi";
 
 export default function Header() {
   return (
-    <header className="" >
+    <header >
       <div className=" max-w-7xl  mx-auto px-8  ">
         <div className=" flex justify-between items-center gap-2.5 py-3" >
           <div className="flex items-center">
--- src/components/result-screen.tsx
+++ src/components/result-screen.tsx
@@ -1,3 +1,4 @@
+import { FaTrophy } from "react-icons/fa";
 
 enum ResultStatusEnum {
   FAILED = "FAILED",
@@ -11,30 +12,44 @@ export default function ResultScreen(props: {
   const highScore = 0;
   let result: {
     status: ResultStatusEnum,
-    message: string
+    title: string,
+    subTitle: string
   } | null = null;
 
   if (props.totalScore < 10) {
     result = {
-      message: "You are Failed!",
+      title: "Failed",
+      subTitle: "You are Failed!",
       status: ResultStatusEnum.FAILED
     }
 
   } else if (props.totalScore >= highScore) {
     result = {
-      message: "Bravo! You made a new Highscore",
+      title: "Highscore",
+      subTitle: "Bravo! You made a new Highscore",
       status: ResultStatusEnum.HIGHSCORE
     }
   } else {
     result = {
-      message: "You are Passed!",
+      title: "Congratulation",
+      subTitle: "You are Passed!",
       status: ResultStatusEnum.PASSED
     }
   }
 
 
   return (
+    <section className="card">
+      <div className="flex items-center justify-center flex-col gap-6">
+        <div className="text-5xl ">
+          <FaTrophy />
+        </div>
 
-    <div>{result.message} {highScore}</div>
+        <h1 className="text-2xl font-medium">Congratulation</h1>
+        <p>Your Score</p>
+        <span className="text-3xl">4 / 10</span>
+        <p>You did a great job. Learn more by taking another quiz</p>
+      </div>
+    </section>
   )
 }
